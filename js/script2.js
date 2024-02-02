72f810d6ed1cb23a7bd0cd0cec4b2830d2d8c876
Igho Manny Ibuje <imigreat@yahoo.com>
2024-02-02 01:18:37 +0000

    Added index2.html and script2.js as second API files

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,14 @@
-const url = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=outkast';
 
 async function searchSong(){
+
+// Empty elements before new data
+$("#artist-tracks").empty();
+const trackName = $("#artist-name").val().trim();
+if (!trackName) {
+    return;
+}
+
+const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${trackName}`;
 const options = {
 	method: 'GET',
 	headers: {
@@ -12,9 +20,31 @@ const options = {
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
-	console.log(result);
+
+    $('#artist-photo').empty()
+    $('#artist-details').empty()
+
+	    //console.log(result.data[0].album.cover_big);
+    var artistImage = $(`<img src="${result.data[0].album.cover_big}" height="450" width="400" />`)
+    $('#artist-photo').append(artistImage)
+
+   //console.log(result.data[0].album.title);
+      var albumTitle = $(`<h2>ARTIST NAME:</h2><h4>${result.data[0].artist.name}</h4>`)
+   $('#artist-details').append(albumTitle)
+
+   //console.log(result.data[0].preview);
+   $('#artist-tracks').append('<h2><br/>TRACKS:</h2>')
+   for(i = 0; i < result.data.length; i++){
+    $('#artist-tracks').append(`<h4>${result.data[i].title} [${i + 1}]</h4>`)
+   $('#artist-tracks').append(`<iframe src="${result.data[i].preview}"></iframe>`);
+   }
+
+    console.log(result.data);
+
 } catch (error) {
 	console.error(error);
 }
 }
 console.log(searchSong())
+
+$(document).on("click", "#search-artist", searchSong);
